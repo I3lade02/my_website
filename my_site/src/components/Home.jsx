50b6a96bea4d2bc79b6edca511e27916ca5efc91
I3lade02 <ondrej.ber@email.cz>
2024-10-19 21:34:57 +0200

    initial commit

---
+++ my_site/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+    return (
+        <div id='#home'>
+            <h1 className='text-center'>Vítejte na mých webových stránkách</h1>
+        </div>
+    );
+};
+
+export default Home;
